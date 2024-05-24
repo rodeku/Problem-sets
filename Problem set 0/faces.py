f9427f957bd49b99784594c471519066624105dc
rodeku <37703038+rodeku@users.noreply.github.com>
2024-05-24 13:26:27 +0200

    faces.py solved

--- Problem set 0/faces.py
+++ Problem set 0/faces.py
@@ -1,2 +1,12 @@
-input = input()
-print(input)
+#print("\U0001F642") #Slightly smiling face 🙂
+#print("\U0001F641") #Slightly frowning face 🙁
+
+def convert(string):
+    return string.replace(':)','\U0001F642').replace(':(','\U0001F641')
+
+def main():
+    x = input('Please write your phrase: ')
+    variable = convert(x)
+    print(variable)
+
+main()
